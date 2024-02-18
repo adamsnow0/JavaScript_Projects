026f56ccbd77453f153ef882a2b9cd5c70f13ecc
adamsnow0 <adam.snow12381@gmail.com>
2024-02-17 22:46:52 -0800

    Index file with JavaScript
    Created a new html Index file and applied a JavaScript source file to it.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+document.write("Hello, World! "); 
+
+var A = "Welcome to this webpage we call \'home\'.";
+document.write(A);
+
+document.write("<br><br>We're so glad to have you here.<br><br>As my dad always says, \"Mi casa es su casa!\"<br>")
+
+document.write("<br>\"The magic thing about home is that it feels good to leave,"
++ " and it feels even better to come back.\""
++ " - Wendy Wunder, Author<br><br>");
+
+var Myself = "Adam", Husband = "Doug", Cat1 = "Willow", Cat2 = "Jasper", Cat3 = "Colby";
+document.write(Cat2); 
