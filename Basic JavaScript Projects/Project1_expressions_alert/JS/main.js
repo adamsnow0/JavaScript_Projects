3e05ed81085ee86e5b4cbe362d13fd30812625cb
adamsnow0 <adam.snow12381@gmail.com>
2024-02-21 19:15:23 -0800

    Adding comments
    Added comments to main.js file to explain what the code is doing.

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,13 +1,13 @@
-document.write("Hello, World! "); 
+document.write("Hello, World! "); //Adding text into HTML output
 
-var A = "Welcome to this webpage we call \'home\'.";
-document.write(A);
+var A = "Welcome to this webpage we call \'home\'."; //This is defining a variable
+document.write(A); //Writing a specifically defined variable into HTML output
 
 document.write("<br><br>We're so glad to have you here.<br><br>As my dad always says, \"Mi casa es su casa!\"<br>")
 
 document.write("<br>\"The magic thing about home is that it feels good to leave,"
 + " and it feels even better to come back.\""
-+ " - Wendy Wunder, Author<br><br>");
++ " - Wendy Wunder, Author<br><br>"); //Concatenating a string
 
-var Myself = "Adam", Husband = "Doug", Cat1 = "Willow", Cat2 = "Jasper", Cat3 = "Colby";
-document.write(Cat2); 
+var Myself = "Adam", Husband = "Doug", Cat1 = "Willow", Cat2 = "Jasper", Cat3 = "Colby"; //Defining multiple variables
+document.write(Cat2); //Writing a specifically defined variable into HTML output
