5257fcdd4a737081071d45e3f399235c77678d9d
adamsnow0 <adam.snow12381@gmail.com>
2024-02-26 21:52:01 -0800

    Updates to HTML and JS file
    Function added to JS file and HTML updated.

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    // string utilizing += operator to concatenate it
+    var sentence = "This is my favorite";
+    sentence += " show to watch online!";
+    // method to display concatenated string
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
