8bf122c17fec99b0afb6556d73cbe3c925ad9a12
adamsnow0 <adam.snow12381@gmail.com>
2024-03-12 23:20:07 -0700

    JavaScript Project 6
    Writing HTML code and corresponding JavaScript functions utilizing ternary operators

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,3 +1,4 @@
+//function utilizing ternary operator
 function Ride_Function() {
     var Height, Can_ride;
     Height = document.getElementById("Height").value;
@@ -5,9 +6,18 @@ function Ride_Function() {
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
 
+//function utilizing ternary operator
 function Vote_Function() {
     var Age, Can_vote;
-    Age = document.getElementById("Age").value;
+    Age = document.getElementById("Age_1").value;
     Can_vote = (Age < 18) ? "We're sorry. You are not yet old enough":"Congrats! You are old enough";
     document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
+}
+
+//function utilizing ternary operator
+function Drink_Function() {
+    var Age, Can_drink;
+    Age = document.getElementById("Age_2").value;
+    Can_drink = (Age < 21) ? "So sorry, but you're not yet old enough":"Welcome! You are old enough";
+    document.getElementById("Drink").innerHTML = Can_drink + " to drink."
 }
